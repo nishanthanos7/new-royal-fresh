59f396cf92daa763cca8e01ab9437b93087c2ba0
nishanThanos7 <nishan.poudel12345@gmail.com>
2026-06-14 12:13:09 +0545

    build error node

--- new-royal/api/images.ts
+++ new-royal/api/images.ts
@@ -1,6 +1,6 @@
 import { put, list } from '@vercel/blob';
 
-export const config = { runtime: 'nodejs20.x' };
+export const config = { runtime: 'nodejs' };
 
 const BLOB_PATHNAME = 'config/images.json';
 
